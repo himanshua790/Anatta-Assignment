d8e4c0f879c27dac68ad3783500dfffc474385ca
Himanshu Soni <himanshua790@gmail.com>
2023-07-20 02:08:48 +0530

    feat: added pagination to products api

--- utils.js
+++ utils.js
@@ -4,49 +4,100 @@ const axios = require("axios");
 
 async function fetchProducts(query, first) {
   console.log(`\nquery: ${query}, first:${first}\n`);
-  const data = JSON.stringify({
-    query: `query searchProducts($query: String!, $first: Int) {
-      search(query: $query, first: $first, types: PRODUCT) {
-        edges {
-          node {
-            ... on Product {
-              id
-              title
-              variants(first: 1) {
-    
-                edges {
-                  node {
+
+  const batchSize = 50;
+  const batches = createBatches(first, batchSize);
+  const output = [];
+
+  const fetchBatch = async (query, first, after = null) => {
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `https://${process.env.SHOP}/api/2023-07/graphql.json`,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token":
+          process.env.STOREFRONT_ACCESS_TOKEN,
+      },
+    };
+
+    const variables = {
+      query,
+      first,
+      after,
+    };
+
+    const data = JSON.stringify({
+      query: `query searchProducts($query: String!, $first: Int, $after: String) {
+            search(query: $query, first: $first, after: $after, types: PRODUCT) {
+              edges {
+                cursor
+                node {
+                  ... on Product {
+                    id
                     title
-                    
-                    price {
-                      amount
+                    variants(first: 1) {
+                      edges {
+                        node {
+                          title
+                          price {
+                            amount
+                          }
+                        }
+                      }
                     }
                   }
                 }
               }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
             }
           }
+          `,
+      variables,
+    });
+
+    try {
+      const response = await axios.request({ ...config, data });
+      if (response?.status === 200) {
+        const searchResults = response?.data?.data?.search;
+        if (searchResults) {
+          const hasNextPage = searchResults?.pageInfo?.hasNextPage;
+          const endCursor = searchResults?.pageInfo?.endCursor;
+          return {
+            data: searchResults?.edges?.map((n) => n?.node) || [],
+            endCursor,
+            hasNextPage,
+          };
         }
       }
+    } catch (error) {
+      console.error("Error fetching records:", error.message);
     }
-    `,
-    variables: { query, first },
-  });
-
-  const config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: `https://${process.env.SHOP}/api/2023-07/graphql.json`,
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Storefront-Access-Token": process.env.STOREFRONT_ACCESS_TOKEN,
-    },
-    data: data,
+    return [];
   };
 
-  const resp = await axios.request(config);
-  return resp?.data?.data?.search?.edges?.map((n) => n?.node) || [];
+  async function fetchAllBatches() {
+    let endCursor = null;
+    for (const batchSize of batches) {
+      const {
+        data: result,
+        endCursor: rawCursor,
+        hasNextPage,
+      } = await fetchBatch(query, batchSize, endCursor);
+      output.push(...result);
+      endCursor = rawCursor;
+      if (!hasNextPage) break;
+    }
+  }
+
+  await fetchAllBatches();
+
+  return output;
 }
+
 function fetchDataFromProducts(products) {
   return products?.map((product) => ({
     title: product?.title,
@@ -54,4 +105,18 @@ function fetchDataFromProducts(products) {
     price: Number(product?.variants?.edges?.[0]?.node?.price?.amount) || 0,
   }));
 }
+
+function createBatches(totalRecordsToFetch, paginationLimit) {
+  const batches = [];
+  let remainingRecords = totalRecordsToFetch;
+
+  while (remainingRecords > 0) {
+    const batchSize = Math.min(remainingRecords, paginationLimit);
+    batches.push(batchSize);
+    remainingRecords -= batchSize;
+  }
+
+  return batches;
+}
+
 module.exports = { fetchProducts, fetchDataFromProducts };
